7bd00838e4d64af5b66c7edfa72f5a70c70b0e35
Dwayne Litzenberger <dlitz@inboxapp.com>
2014-09-19 18:11:28 -0700

    snapshot -- Friday demo

--- inbox/util/integrity_check_imap.py
+++ inbox/util/integrity_check_imap.py
@@ -10,12 +10,15 @@ Run as:
 
 from __future__ import absolute_import, division, print_function
 import argparse
+import errno
 import httplib2
+import itertools
+import os
 from apiclient.discovery import build
 from oauth2client.client import OAuth2Credentials
 from inbox.log import configure_logging, get_logger
 from imapclient import IMAPClient
-from inbox.models import Account, Message, Namespace
+from inbox.models import Account, Folder, FolderItem, Message, Namespace, Thread
 from inbox.models.session import session_scope
 from inbox.models.backends.gmail import GmailAccount
 from inbox.models.backends.imap import ImapThread
@@ -24,11 +27,84 @@ from inbox.sqlalchemy_ext.util import b36_to_bin, int128_to_b36 as bin_to_b36
 from inbox.auth.gmail import (OAUTH_CLIENT_ID,
                               OAUTH_CLIENT_SECRET,
                               OAUTH_ACCESS_TOKEN_URL)
+from inbox.util.addr import parse_email_address_list
 import sqlalchemy as sa
 from sqlalchemy.orm import object_session
 import sqlite3
 
 SOURCE_APP_NAME = 'Testing the Gmail API'
+
+
+SQLITE3_INIT_SCRIPT = r"""
+    CREATE TABLE messages (
+        id INTEGER NOT NULL PRIMARY KEY,
+        date TEXT,
+        subject TEXT,
+        size INTEGER,
+        in_reply_to TEXT,
+        message_id_header TEXT,
+        x_gm_msgid TEXT UNIQUE,
+        x_gm_thrid TEXT
+    );
+    CREATE INDEX ix_messages_message_id_header ON messages(message_id_header);
+    CREATE INDEX ix_messages_x_gm_thrid ON messages(x_gm_thrid);
+    CREATE INDEX ix_messages_x_gm_msgid ON messages(x_gm_msgid);
+
+    -- TODO
+    -- -- Info attached to messages where there are multiple values
+    -- -- (e.g. To, Cc, & other headers)
+    -- CREATE TABLE message_info (
+    --     id INTEGER NOT NULL PRIMARY KEY,
+    --     message_id INTEGER NOT NULL,
+    --     name TEXT NOT NULL,
+    --     value TEXT
+    -- );
+
+    CREATE TABLE threads (
+        id INTEGER NOT NULL PRIMARY KEY,
+        x_gm_thrid TEXT UNIQUE
+    );
+
+    CREATE TABLE thread_messages (
+        thread_id INTEGER NOT NULL,
+        message_id INTEGER NOT NULL,
+        PRIMARY KEY(thread_id, message_id)
+    );
+
+    CREATE TABLE folders (
+        folder_name TEXT NOT NULL PRIMARY KEY,
+        imap_delimiter TEXT,
+        imap_uidvalidity INTEGER,
+        imap_uidnext INTEGER,
+        imap_noselect BOOLEAN
+    );
+
+    CREATE TABLE folder_messages (
+        folder_name TEXT NOT NULL,
+        imap_uid INTEGER NOT NULL,
+        message_id INTEGER,
+        PRIMARY KEY(folder_name, imap_uid)
+    );
+
+    -- Raw gmail labels are different per-message.
+    CREATE TABLE folder_message_gm_labels (
+        folder_name TEXT NOT NULL,
+        message_id INTEGER NOT NULL,
+        label TEXT NOT NULL,
+        PRIMARY KEY(folder_name, message_id, label)
+    );
+
+    CREATE TABLE folder_flags (
+        folder_name TEXT NOT NULL,
+        flag TEXT NOT NULL,
+        PRIMARY KEY(folder_name, flag)
+    );
+
+    CREATE TABLE special_folders (
+        attr_name TEXT NOT NULL PRIMARY KEY,
+        folder_name TEXT NOT NULL UNIQUE
+    );
+"""
 
 
 class GmailThread(object):
@@ -243,96 +319,61 @@ def dump_namespace(namespace, account):
 #                    imapuid.msg_uid, imapuid.folder.name))
 # TODO
 
-    # XXX: We might need to spill these to disk.
+    try:
+        os.unlink('local.db')
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
 #    db_local = sqlite3.connect(':memory:')
-    db_imap = sqlite3.connect(':memory:')
+    db_local = sqlite3.connect('local.db')
+    db_local.row_factory = sqlite3.Row
+    db_local.executescript(SQLITE3_INIT_SCRIPT)
+    slurp_local_namespace(namespace, account, db=db_local)
+
+    try:
+        os.unlink('imap.db')
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+#    db_imap = sqlite3.connect(':memory:')
+    db_imap = sqlite3.connect('imap.db')
     db_imap.row_factory = sqlite3.Row
-    db_imap.executescript("""
-        CREATE TABLE messages (
-            id INTEGER NOT NULL PRIMARY KEY,
-            subject TEXT
-        );
-
-        CREATE TABLE folders (
-            id INTEGER NOT NULL PRIMARY KEY,
-            folder_name TEXT NOT NULL,
-            imap_uidvalidity INTEGER
-        );
-
-        CREATE TABLE folder_messages (
-            message_id INTEGER NOT NULL,
-            folder_id INTEGER NOT NULL,
-            imap_uid INTEGER,
-            PRIMARY KEY(message_id, folder_id)
-        );
-
-        CREATE TABLE folder_flags (
-            id INTEGER NOT NULL PRIMARY KEY,
-            folder_id INTEGER NOT NULL,
-            flag TEXT NOT NULL
-        );
-        """)
-
-
-
-#    slurp_local_namespace(namespace, account, db=db_local)
-
+    db_imap.executescript(SQLITE3_INIT_SCRIPT)
     slurp_imap_namespace(namespace, account, db=db_imap)
-#    dump_imap_namespace(namespace, account)   #  DEBUG FIXME(dlitz)   
 
+def slurp_local_namespace(namespace, account, db):
+    info = provider_info(account.provider)
 
+    if account.provider == 'gmail':
+        slurp_local_namespace_gmail(namespace=namespace, account=account, db=db)
+    else:
+        raise NotImplementedError(
+            "only gmail-style accounts currently supported")
 
-    # TODO:
-    # get local list of threads
-    # get local list of tags (?)
-    # get remote list of folders
-    # get remote list of threads
-    #db_session.query(
 
 
 def slurp_imap_namespace(namespace, account, db):
     info = provider_info(account.provider)
 
-    conn = IMAPClient(info['imap'], use_uid=True, ssl=True)
-    #conn.debug = True       #  DEBUG FIXME(dlitz)   
+    imap = IMAPClient(info['imap'], use_uid=True, ssl=True)
+    imap.debug = True       #  DEBUG FIXME(dlitz)   
     if info['auth'] == 'oauth2':
-        conn.oauth2_login(account.email_address, account.access_token)
+        imap.oauth2_login(account.email_address, account.access_token)
     elif info['auth'] == 'password':
-        conn.login(account.email_address, account.password)
+        imap.login(account.email_address, account.password)
     else:
         raise NotImplementedError(
             "auth mechanism {0!r} not implemented for provider {1!r}".format(
                 info['auth'], account.provider))
 
     if account.provider == 'gmail':
-        slurp_imap_namespace_gmail(conn, namespace=namespace, account=account, db=db)
+        slurp_imap_namespace_gmail(imap, namespace=namespace, account=account, db=db)
     else:
         raise NotImplementedError(
             "only gmail-style accounts currently supported")
 
 
-#def dump_imap_namespace(namespace, account):
-#    info = provider_info(account.provider)
-#
-#    conn = IMAPClient(info['imap'], use_uid=True, ssl=True)
-#    #conn.debug = True       #  DEBUG FIXME(dlitz)   
-#    if info['auth'] == 'oauth2':
-#        conn.oauth2_login(account.email_address, account.access_token)
-#    elif info['auth'] == 'password':
-#        conn.login(account.email_address, account.password)
-#    else:
-#        raise NotImplementedError(
-#            "auth mechanism {0!r} not implemented for provider {1!r}".format(
-#                info['auth'], account.provider))
-#
-#    if account.provider == 'gmail':
-#        dump_imap_namespace_gmail(conn, namespace=namespace, account=account)
-#    else:
-#        raise NotImplementedError(
-#            "only gmail-style accounts currently supported")
-
-
-def slurp_imap_namespace_gmail(conn, db, namespace=None, account=None):
+def slurp_imap_namespace_gmail(imap, db, namespace=None, account=None):
     # folder attrs -> RFC 6154 Special-Use mailbox flags
     singleton_flags = {
         'all_folder': u'\\All',
@@ -345,52 +386,223 @@ def slurp_imap_namespace_gmail(conn, db, namespace=None, account=None):
     }
 
     # List folders -- Returns sequence of (flags, delimiter, name)
-    folders_fdn = conn.list_folders()
+    folders_fdn = imap.list_folders()
+    with db:
+        # Folder names & delimiters
+        db.executemany(
+            "INSERT INTO folders (folder_name, imap_delimiter) VALUES (?, ?)",
+            ((name, delimiter) for flags, delimiter, name in folders_fdn))
 
-    # Compute set of folders for each flag
-    folders_by_flag = {}
-    for flags, delimiter, name in folders_fdn:
-        for flag in flags:
-            folders_by_flag.setdefault(flag, set())
-            folders_by_flag[flag].add(name)
+        # Folder flags
+        db.executemany("""
+            INSERT INTO folder_flags (folder_name, flag) VALUES (?, ?)
+            """, ((name, flag)
+                  for flags, delimiter, name in folders_fdn
+                  for flag in flags))
 
-    # Output the folder names for each special folder.
-    folder_attrs = sorted(list(singleton_flags) + ['inbox_folder'])
-    for folder_attr in folder_attrs:
-        if folder_attr == 'inbox_folder':
-            folder_names = list(name for flags, delimiter, name in folders_fdn)
-            if u'INBOX' not in folder_names:
-                print("** warning: no INBOX folder")
-            else:
-                print('account.{0}.name: {1}'.format(folder_attr, u'INBOX'))
-        else:
-            flag = singleton_flags[folder_attr]
-            folders = folders_by_flag.get(flag, ())
-            if len(folders) > 1:
-                print("** warning: Multiple folders for flag {0}: {1!r}".format(
-                    flag, sorted(folders_by_flag[flag])))
-            elif len(folders) == 0:
-                print("account.{0}: {1}".format(folder_attr, None))
-            for folder in folders:
-                print("account.{0}.name: {1}".format(folder_attr, folder))
+        # Set imap_noselect = 1 on folders that have the \Noselect flag;
+        # Set imap_noselect = 0 on folders that don't.
+        db.execute("""
+            UPDATE folders SET imap_noselect = (
+                SELECT folder_flags.flag IS NOT NULL
+                FROM folders AS a LEFT JOIN folder_flags ON (
+                    a.folder_name = folder_flags.folder_name AND
+                    folder_flags.flag = '\Noselect'
+                )
+                WHERE folders.folder_name = a.folder_name
+            )
+            """)
 
-    # TODO Open the "Trash" folder and slurp the list of messages
-    # TODO Open the "All Mail" folder and slurp the list of messages
+        # Insert 'inbox_folder' -> 'INBOX' if there is an INBOX folder, which
+        # there should always be, I think.
+        db.execute("""
+            INSERT INTO special_folders (attr_name, folder_name)
+            SELECT ?, folder_name FROM folders WHERE folder_name = ?
+            """, ['inbox_folder', 'INBOX'])
 
-    dbc = db.cursor()
+        # Insert other special folder names
+        db.executemany("""
+            INSERT INTO special_folders (attr_name, folder_name)
+            SELECT ?, folder_name FROM folder_flags WHERE flag = ?
+            """, singleton_flags.items())
 
-    for folder in folders_by_flag[u'\\All'] + folders_by_flag[u'\\Trash']:
-        select_response = conn.select_folder(folder)
-        uids = conn.search()
-        dbc.execute(
-            "INSERT INTO folders (folder_name, imap_uidvalidity) VALUES (?, ?)"
-            (folder, select_response[u'UIDVALIDITY']))
+    # Fetch all messages from each folder
+    with db:
+        folder_names = [row[0] for row in db.execute(
+            "SELECT folder_name FROM folders WHERE NOT imap_noselect")]
+
+        for folder_name in folder_names:
+            # EXAMINE the folder
+            examine_response = imap.select_folder(folder_name, readonly=True)
+
+            # Update imap_uidvalidity
+            db.execute("""
+                UPDATE folders
+                SET imap_uidvalidity = ?, imap_uidnext = ?
+                WHERE folder_name = ?
+                """, [examine_response[u'UIDVALIDITY'],
+                      examine_response[u'UIDNEXT'],
+                      folder_name])
+
+            # Get uids of the messages in the folder
+            imap_uids = imap.search()
+
+            # Result should match the stated number of messages in the folder.
+            if len(imap_uids) != examine_response[u'EXISTS']:
+                raise AssertionError("len(imap_uids)={0}, EXISTS={1!r}".format(
+                    len(imap_uids), examine_response[u'EXISTS']))
+
+            # Create folder_messages entries
+            db.executemany("""
+                INSERT INTO folder_messages (folder_name, imap_uid, message_id)
+                VALUES (?, ?, NULL)
+                """, ((folder_name, imap_uid) for imap_uid in imap_uids))
+
+            # Get the folder flags
+            folder_flags = set(row[0] for row in db.execute(
+                "SELECT flag FROM folder_flags WHERE folder_name = ?",
+                [folder_name]))
+
+            # This is Gmail, so only actually fetch messages from the 'All
+            # Mail' and 'Trash' folders.  This *should* give us all of the
+            # messages.
+            #if not folder_flags & {u'\\All', u'\\Trash', u'\\Sent'}:
+            #    continue
+
+            # Get folder messages
+            batch_size = 1000
+            fetch_data = ['RFC822.SIZE', 'ENVELOPE', 'FLAGS',
+                          'X-GM-MSGID', 'X-GM-THRID', 'X-GM-LABELS']
+            for i in range(0, len(imap_uids), batch_size):
+                imap_uids_batch = imap_uids[i:i+batch_size]
+
+                # Fetch message info from the IMAP server
+                fetch_response = imap.fetch(imap_uids_batch, fetch_data)
+
+                # Fetch message info and insert it into the messages table.
+                # Don't bother deduplicating at this point.
+                for uid, data in fetch_response.items():
+                    msg_data = dict(
+                        date=data['ENVELOPE'].date,
+                        subject=data['ENVELOPE'].subject,
+                        in_reply_to=data['ENVELOPE'].in_reply_to,
+                        size=data['RFC822.SIZE'],
+                        message_id_header=data['ENVELOPE'].message_id,
+                        x_gm_thrid=unicode(data['X-GM-THRID']),
+                        x_gm_msgid=unicode(data['X-GM-MSGID']),
+                    )
+
+                    # TODO
+                    #msg_info_data = list(itertools.chain(
+                    #    [('from', a) for a in data['ENVELOPE'].from_ or ()],
+                    #    [('sender', a) for a in data['ENVELOPE'].sender or ()],
+                    #    [('reply_to', a) for a in data['ENVELOPE'].reply_to or ()],
+                    #    [('to', a) for a in data['ENVELOPE'].to or ()],
+                    #    [('cc', a) for a in data['ENVELOPE'].cc or ()],
+                    #    [('bcc', a) for a in data['ENVELOPE'].bcc or ()],
+                    #))
+
+                    # Check if we've already stored the message
+                    cur = db.execute("""
+                        SELECT id, x_gm_msgid FROM messages
+                        WHERE x_gm_msgid = :x_gm_msgid
+                        """, msg_data)
+                    row = next(iter(cur.fetchall()), None)    # returns 0 or 1 rows
+                    message_id = row['id'] if row is not None else None
+
+                    # If we've never stored the message, store it now.
+                    if message_id is None:
+                        cur = db.execute("""
+                            INSERT INTO messages (
+                                date, subject, in_reply_to, size,
+                                message_id_header, x_gm_msgid, x_gm_thrid
+                            ) VALUES (
+                                :date, :subject, :in_reply_to, :size,
+                                :message_id_header, :x_gm_msgid, :x_gm_thrid
+                            )
+                            """, msg_data)
+                        message_id = cur.lastrowid
+
+                        # TODO
+                        #db.executemany("""
+                        #    INSERT INTO message_info (message_id, name, value)
+                        #    VALUES (?, ?, ?)
+                        #    """,
+                        #    [(message_id, name, value)
+                        #     for name, value in msg_info_data])
+
+                    # Store the Gmail labels (these can be different in
+                    # different folders; e.g. messages in the 'Sent' folder are
+                    # missing the u'\\Sent' label)
+                    db.executemany("""
+                        INSERT INTO folder_message_gm_labels
+                            (folder_name, message_id, label)
+                        VALUES (?, ?, ?)
+                        """,
+                        ((folder_name, message_id, label)
+                         for label in data['X-GM-LABELS']))
+
+                    # Mark the message as being in the current folder.
+                    db.execute("""
+                        UPDATE folder_messages
+                        SET message_id = ?
+                        WHERE folder_name = ? AND imap_uid = ?
+                        """, (message_id, folder_name, uid))
+
+        # Construct threads (assuming gmail for now)
+        db.execute("""
+            INSERT INTO threads (x_gm_thrid)
+            SELECT DISTINCT x_gm_thrid FROM messages
+            """)
+        db.execute("""
+            INSERT INTO thread_messages (thread_id, message_id)
+            SELECT threads.id, messages.id
+            FROM threads, messages
+            WHERE threads.x_gm_thrid = messages.x_gm_thrid
+            """)
 
 
+def slurp_local_namespace_gmail(db, namespace=None, account=None):
+    db_session = object_session(namespace)
 
+    with db:
+        # Insert folders
+        db.executemany("""
+            INSERT INTO folders (folder_name, imap_uidvalidity)
+            VALUES (?, ?)
+            """,
+            ((f.name,
+              f.imapfolderinfo[0].uidvalidity if f.imapfolderinfo else None)
+             for f in account.folders))
 
-    import IPython; IPython.embed() #  DEBUG FIXME(dlitz)   
+        # Fetch threads
+        #threads = (db_session.query(ImapThread.id, ImapThread.g_thrid)
+        #    .filter_by(namespace_id=namespace.id)
+        #    .all())
+        threads = namespace.threads
 
+        # Insert threads
+        db.executemany("""
+            INSERT INTO threads (id, x_gm_thrid) VALUES (?, ?)
+            """, ((thread.id, thread.g_thrid) for thread in threads))
+
+        # Keep thread ids
+        thread_ids = [thread.id for thread in threads]
+        del threads
+
+        # Slurp messages in batches
+        batch_size = 1000
+        for i in range(0, len(thread_ids), batch_size):
+            thread_ids_batch = thread_ids[i:i+batch_size]
+            rows = (
+                db_session.query(Message.id, Message.g_thrid, Message.g_msgid,
+                    Message.received_date, Message.subject, Message.size)
+                .filter(Message.thread_id.in_(thread_ids_batch))
+                .all())
+            db.executemany("""
+                INSERT INTO messages (id, x_gm_thrid, x_gm_msgid, date, subject, size)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """, rows)
 
 
 def main():
