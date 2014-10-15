5cbece4a38922f62061519485327fafee5ecdf20
karim hamidou <khamidou@inboxapp.com>
2014-10-15 15:07:03 +0200

    updated db dump

--- migrations/versions/4634999269_store_label_information_per_uid.py
+++ migrations/versions/4634999269_store_label_information_per_uid.py
@@ -8,7 +8,7 @@ Create Date: 2014-10-14 10:04:58.710015
 
 # revision identifiers, used by Alembic.
 revision = '4634999269'
-down_revision = '5709063bff01'
+down_revision = '26911668870a'
 
 from alembic import op
 import sqlalchemy as sa
@@ -17,9 +17,9 @@ from inbox.sqlalchemy_ext.util import JSON
 
 
 def upgrade():
-    op.add_column('imapuid', sa.Column('labels', JSON(),
+    op.add_column('imapuid', sa.Column('g_labels', JSON(),
                                        nullable=False))
 
 
 def downgrade():
-    op.drop_column('imapuid', 'labels')
+    op.drop_column('imapuid', 'g_labels')
