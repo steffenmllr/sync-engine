steffenmllr/sync-engine

commit e9be738262afadd4bd723be16dd1c3b4c46da041

Implement synchronous sending.
Summary:
Fixes T730. Instead of scheduling a sending action, the API now attempts to
actually send the message before returning. An appropriate status code is
returned if sending fails.

Various cleanups are included as a result of this work:
 * Consolidate `BaseSMTPClient` and `SMTPClient` classes. There was no reason
   to have a separate abstract base class.

 * Consolidate input validation in draft update/delete/send endpoints
   into a single `get_draft()` function.

 * Consolidate `send_new/send_reply` and `create_email/create_reply` methods
   into just `send` and `create_email`.

 * Simplify the construction of a MIME message from a draft object.
   `inbox.sendmail.message.create_email` now just gives you a string
   representing an RFC2821-compliant, ready-for-action MIME message.

 * Update a thread's `last_message_timestamp` attribute after a draft on it is
   sent (Fixes T604).

Test Plan: Substantial unit tests added.

Reviewers: kav-ya

Reviewed By: kav-ya

Maniphest Tasks: T604, T730

Differential Revision: https://review.inboxapp.com/D1076

inbox/sendmail/base.py
@@ -1,32 +1,30 @@
-from datetime import datetime, timedelta
-from sqlalchemy import func
-from inbox.config import config
+from datetime import datetime
 from inbox.contacts.process_mail import update_contacts_from_message
-from inbox.models import Message, Thread, Part, ActionLog
+from inbox.models import Message, Part
 from inbox.models.action_log import schedule_action
 from inbox.sqlalchemy_ext.util import generate_public_id
 
 
-DEFAULT_DAILY_SENDING_LIMIT = 300
-
-
 class SendMailException(Exception):
-    pass
-
-
-class SendError(SendMailException):
-    def __init__(self, msg=None, failures=None):
-        assert msg or failures
-        self.msg = msg
+    """
+    Raised when sending fails.
+    Parameters
+    ----------
+    message: string
+        A descriptive error message.
+    http_code: int
+        An appropriate HTTP error code for the particular type of failure.
+    failures: dict, optional
+        If sending only failed for some recipients, information on the specific
+        failures.
+    """
+    def __init__(self, message, http_code, failures=None):
+        self.message = message
+        self.http_code = http_code
         self.failures = failures
 
     def __str__(self):
-        if not self.failures:
-            return 'Send failed, message: {0}'.format(self.msg)
-
-        e = ['(to: {0}, error: {1})'.format(k, v[0]) for
-             k, v in self.failures.iteritems()]
-        return 'Send failed, failures: {0}'.format(', '.join(e))
+        return self.message
 
 
 def get_sendmail_client(account):
@@ -34,15 +32,8 @@ def get_sendmail_client(account):
 
     sendmail_mod = module_registry.get(account.provider)
     sendmail_cls = getattr(sendmail_mod, sendmail_mod.SENDMAIL_CLS)
-    sendmail_client = sendmail_cls(account.id)
+    sendmail_client = sendmail_cls(account)
     return sendmail_client
-
-
-def get_draft(db_session, account, draft_public_id):
-    """ Get the draft with public_id = `draft_public_id`, or None. """
-    return db_session.query(Message).join(Thread).filter(
-        Message.public_id == draft_public_id,
-        Thread.namespace_id == account.namespace.id).first()
 
 
 def create_draft(db_session, account, to_addr=None, subject=None,
@@ -70,23 +61,7 @@ def update_draft(db_session, account, original_draft, to_addr=None,
                  subject=None, body=None, blocks=None, cc_addr=None,
                  bcc_addr=None, tags=None):
     """
-    Update draft.
-
-    To maintain our messages are immutable invariant, we create a new draft
-    message object.
-
-
-    Returns
-    -------
-    Message
-        The new draft message object.
-
-    Notes
-    -----
-    Messages, including draft messages, are immutable in Inbox.
-    So to update a draft, we create a new draft message object and
-    return its public_id (which is different than the original's).
-
+    Update draft with new attributes.
     """
 
     def update(attr, value=None):
@@ -135,6 +110,9 @@ def update_draft(db_session, account, original_draft, to_addr=None,
     original_draft.contacts = []
     update_contacts_from_message(db_session, original_draft, account.namespace)
 
+    # Sync to remote
+    schedule_action('save_draft', original_draft, original_draft.namespace.id,
+                    db_session)
     # Delete previous version on remote
     schedule_action('delete_draft', original_draft,
                     original_draft.namespace.id, db_session,
@@ -146,22 +124,15 @@ def update_draft(db_session, account, original_draft, to_addr=None,
     update('version', version)
     update('inbox_uid', version)
 
-    # Sync to remote
-    schedule_action('save_draft', original_draft, original_draft.namespace.id,
-                    db_session)
-
     db_session.commit()
 
     return original_draft
 
 
-def delete_draft(db_session, account, draft_public_id):
-    """ Delete the draft with public_id = `draft_public_id`. """
-    draft = db_session.query(Message).filter(
-        Message.public_id == draft_public_id).one()
+def delete_draft(db_session, account, draft):
+    """ Delete the given draft. """
     thread = draft.thread
     namespace = draft.namespace
-
     assert draft.is_draft
 
     # Delete remotely.
@@ -215,7 +186,6 @@ def create_and_save_draft(db_session, account, to_addr=None, subject=None,
         message.namespace = account.namespace
         message.is_created = True
         message.is_draft = True
-        message.state = 'draft'
         message.from_addr = [(account.name, account.email_address)]
         # TODO(emfree): we should maybe make received_date nullable, so its
         # value doesn't change in the case of a drafted-and-later-reconciled
@@ -301,20 +271,3 @@ def _set_reply_headers(new_message, thread):
                                           [last_message.message_id_header])
             else:
                 new_message.references = [last_message.message_id_header]
-
-
-def rate_limited(namespace_id, db_session):
-    """Check whether sending for the given namespace should be rate-limited.
-    Returns
-    -------
-    bool
-        True if the namespace has exceeded its sending quota.
-    """
-    max_sends = (config.get('DAILY_SENDING_LIMIT') or
-                 DEFAULT_DAILY_SENDING_LIMIT)
-    window_start = datetime.utcnow() - timedelta(seconds=86400)
-    prior_send_actions, = db_session.query(func.count(ActionLog.id)). \
-        filter(ActionLog.namespace_id == namespace_id,
-               ActionLog.created_at > window_start,
-               ActionLog.action.in_(('send_directly', 'send_draft'))).one()
-    return prior_send_actions >= max_sends

tests/api/test_drafts.py
@@ -4,13 +4,11 @@ import json
 import os
 from datetime import datetime
 
-import gevent
 import pytest
 
-from tests.util.base import (patch_network_functions, api_client,
-                             syncback_service, default_account)
+from tests.util.base import api_client, default_account
 
-__all__ = ['patch_network_functions', 'api_client', 'syncback_service']
+__all__ = ['default_account', 'api_client']
 
 NAMESPACE_ID = 1
 
@@ -51,7 +49,6 @@ def test_create_and_get_draft(api_client, example_draft):
     assert len(matching_saved_drafts) == 1
     saved_draft = matching_saved_drafts[0]
 
-    assert saved_draft['state'] == 'draft'
     assert all(saved_draft[k] == v for k, v in example_draft.iteritems())
 
     # Check that thread gets the draft tag
@@ -76,7 +73,6 @@ def test_create_reply_draft(api_client):
 
     drafts = api_client.get_data('/drafts')
     assert len(drafts) == 1
-    assert drafts[0]['state'] == 'draft'
 
     assert thread_public_id == drafts[0]['thread_id']
 
@@ -184,8 +180,7 @@ def test_get_all_drafts(api_client, example_draft):
     assert first_public_id != second_public_id
     assert {first_public_id, second_public_id} == {draft['id'] for draft in
                                                    drafts}
-    assert all(item['state'] == 'draft' and item['object'] == 'draft' for item
-               in drafts)
+    assert all(item['object'] == 'draft' for item in drafts)
 
 
 def test_update_draft(api_client):
@@ -300,32 +295,6 @@ def test_delete_remote_draft(db, api_client):
     assert not drafts
 
 
-def test_send(patch_network_functions, api_client, example_draft,
-              syncback_service, default_account):
-    r = api_client.post_data('/drafts', example_draft)
-    draft_public_id = json.loads(r.data)['id']
-    version = json.loads(r.data)['version']
-
-    r = api_client.post_data('/send',
-                             {'draft_id': draft_public_id,
-                              'version': version})
-
-    # TODO(emfree) do this more rigorously
-    gevent.sleep(0.5)
-
-    drafts = api_client.get_data('/drafts')
-    threads_with_drafts = api_client.get_data('/threads?tag=drafts')
-    assert not drafts
-    assert not threads_with_drafts
-
-    sent_threads = api_client.get_data('/threads?tag=sent')
-    assert len(sent_threads) == 1
-
-    message = api_client.get_data('/messages/{}'.format(draft_public_id))
-    assert message['state'] == 'sent'
-    assert message['object'] == 'message'
-
-
 def test_conflicting_updates(api_client):
     original_draft = {
         'subject': 'parent draft',
@@ -405,16 +374,3 @@ def test_contacts_updated(api_client):
 
     r = api_client.get_data('/threads?to=joe@example.com')
     assert len(r) == 1
-
-
-def test_rate_limiting(patch_network_functions, api_client):
-    """Test that sending is rate-limited appropriately.
-    (Relies on a low value for DAILY_SENDING_LIMIT being set in the test
-    config, for performance.)"""
-    for _ in range(7):
-        r = api_client.post_data('/send', {'to': [{'email':
-                                                   'kermit@example.com'}]})
-        assert r.status_code == 200
-    r = api_client.post_data('/send', {'to': [{'email':
-                                               'kermit@example.com'}]})
-    assert r.status_code == 429

tests/util/base.py
@@ -169,30 +169,13 @@ class TestDB(object):
         self.session.close()
 
 
-class MockSMTPClient(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def send_new(self, db_session, draft, recipients):
-        # Special mock case to test sending failure handling.
-        if 'fail@example.com' in [email for phrase, email in recipients.to]:
-            raise Exception
-        else:
-            pass
-
-    def send_reply(*args, **kwargs):
-        pass
-
-
 @fixture
 def patch_network_functions(monkeypatch):
     """
-    Monkeypatch functions that actually talk to Gmail so that the tests can
-    run faster.
+    Monkeypatch syncback functions that actually talk to Gmail so that the
+    tests can run faster.
 
     """
-    monkeypatch.setattr('inbox.sendmail.base.get_sendmail_client',
-                        lambda *args, **kwargs: MockSMTPClient())
     import inbox.actions
     for backend in inbox.actions.module_registry.values():
         for method_name in backend.__all__:

inbox/actions/__init__.py
@@ -25,16 +25,11 @@ at-least-once semantics.
 # Allow out-of-tree action submodules.
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
-
 from inbox.actions.backends import module_registry
 
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-
 from inbox.models import Account, Message, Thread, Folder
-from inbox.models.action_log import schedule_action
-from inbox.sendmail.base import (generate_attachments, get_sendmail_client,
-                                 SendMailException)
-from inbox.sendmail.message import create_email, Recipients
+from inbox.sendmail.base import generate_attachments
+from inbox.sendmail.message import create_email
 from inbox.log import get_logger
 log = get_logger()
 
@@ -128,19 +123,21 @@ def unmark_trash(account_id, thread_id, db_session):
     set_remote_trash(account, thread_id, False, db_session)
 
 
-def _create_email(account, message, generate_new_uid=False):
-    recipients = Recipients(message.to_addr, message.cc_addr,
-                            message.bcc_addr)
+def _create_email(account, message):
     blocks = [p.block for p in message.attachments]
     attachments = generate_attachments(blocks)
-
-    inbox_uid = message.inbox_uid
-    if generate_new_uid:
-        inbox_uid = None
-
-    return create_email(account.name, account.email_address,
-                        inbox_uid, recipients, message.subject,
-                        message.sanitized_body, attachments)
+    msg = create_email(sender_name=account.name,
+                       sender_email=account.email_address,
+                       inbox_uid=message.inbox_uid,
+                       to_addr=message.to_addr,
+                       cc_addr=message.cc_addr,
+                       bcc_addr=message.bcc_addr,
+                       subject=message.subject,
+                       html=message.sanitized_body,
+                       in_reply_to=message.in_reply_to,
+                       references=message.references,
+                       attachments=attachments)
+    return msg
 
 
 def save_draft(account_id, message_id, db_session):
@@ -166,7 +163,7 @@ def save_draft(account_id, message_id, db_session):
     mimemsg = _create_email(account, message)
     remote_save_draft = module_registry[account.provider].remote_save_draft
     remote_save_draft(account, account.drafts_folder.name,
-                      mimemsg.to_string(), db_session, message.created_at)
+                      mimemsg, db_session, message.created_at)
 
 
 def delete_draft(account_id, draft_id, db_session, args):
@@ -203,93 +200,5 @@ def save_sent_email(account_id, message_id, db_session):
     mimemsg = _create_email(account, message)
     remote_save_sent = module_registry[account.provider].remote_save_sent
     remote_save_sent(account, account.sent_folder.name,
-                     mimemsg.to_string(), message.created_at,
+                     mimemsg, message.created_at,
                      create_backend_sent_folder)
-
-
-def send_directly(account_id, draft_id, db_session):
-    """
-    Send a just-created draft (as opposed to one that was previously created
-    and synced to the backend.
-
-    """
-    _send(account_id, draft_id, db_session)
-
-
-def send_draft(account_id, draft_id, db_session):
-    """Send a previously created draft."""
-    _send(account_id, draft_id, db_session)
-    draft = db_session.query(Message).get(draft_id)
-    # Schedule the deletion separately (we don't want to resend if sending
-    # succeeds but deletion fails!)
-    schedule_action('delete_draft', draft, draft.namespace.id, db_session,
-                    inbox_uid=draft.inbox_uid,
-                    message_id_header=draft.message_id_header)
-
-
-def _send(account_id, draft_id, db_session):
-    """Send the draft with id = `draft_id`."""
-    account = db_session.query(Account).get(account_id)
-
-    try:
-        sendmail_client = get_sendmail_client(account)
-    except SendMailException:
-        log.error('Send Error', message="Failed to create sendmail client.",
-                  account_id=account_id)
-        raise
-    try:
-        draft = db_session.query(Message).filter(
-            Message.id == draft_id).one()
-
-    except NoResultFound:
-        log.info('Send Error',
-                 message='NoResultFound for draft_id {0}'.format(draft_id),
-                 account_id=account_id)
-        raise SendMailException('No draft with id {0}'.format(draft_id))
-
-    except MultipleResultsFound:
-        log.info('Send Error',
-                 message='MultipleResultsFound for draft_id'
-                         '{0}'.format(draft_id),
-                 account_id=account_id)
-        raise SendMailException('Multiple drafts with id {0}'.format(
-            draft_id))
-
-    if not draft.is_draft or draft.is_sent:
-        return
-
-    recipients = Recipients(draft.to_addr, draft.cc_addr, draft.bcc_addr)
-
-    if not draft.is_reply:
-        sendmail_client.send_new(db_session, draft, recipients)
-    else:
-        sendmail_client.send_reply(db_session, draft, recipients)
-
-    if account.provider == 'icloud':
-        # Special case because iCloud doesn't save
-        # sent messages.
-        schedule_action('save_sent_email', draft, draft.namespace.id,
-                        db_session)
-
-    # Update message
-    draft.is_sent = True
-    draft.is_draft = False
-    draft.state = 'sent'
-
-    # Update thread
-    sent_tag = account.namespace.tags['sent']
-    draft_tag = account.namespace.tags['drafts']
-    draft.thread.apply_tag(sent_tag)
-    # Remove the drafts tag from the thread if there are no more drafts.
-    if not draft.thread.drafts:
-        draft.thread.remove_tag(draft_tag)
-
-    return draft
-
-
-def handle_failed_send(account_id, draft_id, db_session):
-    draft = db_session.query(Message).get(draft_id)
-    if draft is None or not draft.is_draft or draft.is_sent:
-        # If object was concurrently deleted or otherwise sent, do nothing.
-        return
-    draft.state = 'sending failed'

inbox/api/err.py
@@ -1,7 +1,11 @@
 from flask import jsonify
 
 
-class InputError(Exception):
+class APIException(Exception):
+    pass
+
+
+class InputError(APIException):
     """Raised on invalid user input (missing required parameter, value too
     long, etc.)"""
     status_code = 400
@@ -10,7 +14,7 @@ class InputError(Exception):
         self.message = message
 
 
-class NotFoundError(Exception):
+class NotFoundError(APIException):
     """Raised when a requested resource doesn't exist."""
     status_code = 404
 
@@ -18,13 +22,17 @@ class NotFoundError(Exception):
         self.message = message
 
 
-def err(http_code, message, code=None, param=None):
+class ConflictError(APIException):
+    status_code = 409
+
+    def __init__(self, message):
+        self.message = message
+
+
+def err(http_code, message, **kwargs):
     resp = {
         'type': 'api_error',
         'message': message
     }
-    if code:
-        resp['code'] = code
-    if param:
-        resp['param'] = param
+    resp.update(kwargs)
     return jsonify(resp), http_code

inbox/api/kellogs.py
@@ -99,8 +99,6 @@ def encode(obj, namespace_public_id=None):
         if obj.is_draft:
             resp['object'] = 'draft'
             resp['version'] = obj.version
-        if obj.state:
-            resp['state'] = obj.state
         return resp
 
     elif isinstance(obj, Thread):

commit 53dd1424cca9373d5f93b17ff2d90830a8e4d454

Properly update thread attributes when a draft is updated.
Summary:
* If the thread only contains the one draft, update its
  subject/recentdate/subjectdate when the draft is updated.
* Replace `original_draft` by `draft` in the `update_draft()` function for
  simplicity, since only one object is being operated on.
* Ensure that the scheduled `delete_draft` action uses the prior inbox_uid
  value. Fixes a regression introduced in commit e9be738.

Fixes T775.

Test Plan: Unit tests updated.

Reviewers: kav-ya

Maniphest Tasks: T775

Differential Revision: https://review.inboxapp.com/D1118

inbox/sendmail/base.py
@@ -57,7 +57,7 @@ def create_draft(db_session, account, to_addr=None, subject=None,
                                  replyto_thread, is_reply, syncback=syncback)
 
 
-def update_draft(db_session, account, original_draft, to_addr=None,
+def update_draft(db_session, account, draft, to_addr=None,
                  subject=None, body=None, blocks=None, cc_addr=None,
                  bcc_addr=None, tags=None):
     """
@@ -66,12 +66,12 @@ def update_draft(db_session, account, original_draft, to_addr=None,
 
     def update(attr, value=None):
         if value is not None:
-            setattr(original_draft, attr, value)
+            setattr(draft, attr, value)
 
             if attr == 'sanitized_body':
                 # Update size, snippet too
-                original_draft.size = len(value)
-                original_draft.snippet = original_draft.calculate_html_snippet(
+                draft.size = len(value)
+                draft.snippet = draft.calculate_html_snippet(
                     value)
 
     update('to_addr', to_addr)
@@ -84,49 +84,58 @@ def update_draft(db_session, account, original_draft, to_addr=None,
     # Remove any attachments that aren't specified
     new_block_ids = [b.id for b in blocks]
     for part in filter(lambda x: x.block_id not in new_block_ids,
-                       original_draft.parts):
-        original_draft.parts.remove(part)
+                       draft.parts):
+        draft.parts.remove(part)
         db_session.delete(part)
 
     # Parts, tags require special handling
     for block in blocks:
         # Don't re-add attachments that are already attached
-        if block.id in [p.block_id for p in original_draft.parts]:
+        if block.id in [p.block_id for p in draft.parts]:
             continue
         part = Part(block=block)
         part.namespace_id = account.namespace.id
         part.content_disposition = 'attachment'
         part.is_inboxapp_attachment = True
-        original_draft.parts.append(part)
+        draft.parts.append(part)
 
         db_session.add(part)
 
-    thread = original_draft.thread
+    thread = draft.thread
+    if len(thread.messages) == 1:
+        # If there are no prior messages on the thread, update its subject and
+        # dates to match the draft.
+        thread.subject = draft.subject
+        thread.subjectdate = draft.received_date
+        thread.recentdate = draft.received_date
+
     if tags:
         tags_to_keep = {tag for tag in thread.tags if not tag.user_created}
         thread.tags = tags | tags_to_keep
 
     # Remove previous message-contact associations, and create new ones.
-    original_draft.contacts = []
-    update_contacts_from_message(db_session, original_draft, account.namespace)
+    draft.contacts = []
+    update_contacts_from_message(db_session, draft, account.namespace)
 
-    # Sync to remote
-    schedule_action('save_draft', original_draft, original_draft.namespace.id,
-                    db_session)
-    # Delete previous version on remote
-    schedule_action('delete_draft', original_draft,
-                    original_draft.namespace.id, db_session,
-                    inbox_uid=original_draft.inbox_uid,
-                    message_id_header=original_draft.message_id_header)
+    prior_inbox_uid = draft.inbox_uid
+    prior_message_id_header = draft.message_id_header
 
     # Update version  + inbox_uid (is_created is already set)
     version = generate_public_id()
     update('version', version)
     update('inbox_uid', version)
 
-    db_session.commit()
+    # Sync to remote
+    schedule_action('save_draft', draft, draft.namespace.id,
+                    db_session)
+    # Delete previous version on remote
+    schedule_action('delete_draft', draft,
+                    draft.namespace.id, db_session,
+                    inbox_uid=prior_inbox_uid,
+                    message_id_header=prior_message_id_header)
 
-    return original_draft
+    db_session.commit()
+    return draft
 
 
 def delete_draft(db_session, account, draft):

tests/api/test_drafts.py
@@ -3,6 +3,7 @@ sending."""
 import json
 import os
 from datetime import datetime
+import gevent
 
 import pytest
 
@@ -185,12 +186,15 @@ def test_get_all_drafts(api_client, example_draft):
 
 def test_update_draft(api_client):
     original_draft = {
-        'subject': 'parent draft',
+        'subject': 'original draft',
         'body': 'parent draft'
     }
     r = api_client.post_data('/drafts', original_draft)
     draft_public_id = json.loads(r.data)['id']
     version = json.loads(r.data)['version']
+
+    # Sleep so that timestamp on updated draft is different.
+    gevent.sleep(1)
 
     updated_draft = {
         'subject': 'updated draft',
@@ -209,6 +213,12 @@ def test_update_draft(api_client):
     drafts = api_client.get_data('/drafts')
     assert len(drafts) == 1
     assert drafts[0]['id'] == updated_public_id
+
+    # Check that the thread is updated too.
+    thread = api_client.get_data('/threads/{}'.format(drafts[0]['thread_id']))
+    assert thread['subject'] == 'updated draft'
+    assert thread['first_message_timestamp'] == drafts[0]['date']
+    assert thread['last_message_timestamp'] == drafts[0]['date']
 
 
 def test_delete_draft(api_client):