5b013aeb8c60449c12f4195e903b07de6bda248c
Karim Hamidou <khamidou@gmail.com>
2015-07-21 20:20:00 +0200

    [iCalendar] Fix a sync-breaking error
    Summary: It seems we've found a special snowflake: a message in the "Sent" folder with no `From:` field. This diff simply adds some code to discard messages like this one.
    
    Test Plan: Not really tested :(
    
    Reviewers: emfree, jennie
    
    Reviewed By: jennie
    
    Maniphest Tasks: T2368
    
    Differential Revision: https://phab.nylas.com/D1778
    
    Conflicts:
    	inbox/events/ical.py

--- inbox/events/ical.py
+++ inbox/events/ical.py
@@ -369,14 +369,21 @@ def import_attached_events(db_session, account, message):
     """Import events from a file into the 'Emailed events' calendar."""
 
     assert account is not None
-    from_addr = message.from_addr[0][1]
+    if message.from_addr is not None:
+        if len(message.from_addr) == 0 or len(message.from_addr[0]) == 1:
+            # We got a message without a from address --- this is either
+            # a message which hasn't been sent or a bogus message. Don't
+            # process it.
+            return
 
-    # FIXME @karim - Don't import iCalendar events from messages we've sent.
-    # This is only a stopgap measure -- what we need to have instead is
-    # smarter event merging (i.e: looking at whether the sender is the
-    # event organizer or not, and if the sequence number got incremented).
-    if from_addr == account.email_address:
-        return
+        from_addr = message.from_addr[0][1]
+
+        # FIXME @karim - Don't import iCalendar events from messages we've sent.
+        # This is only a stopgap measure -- what we need to have instead is
+        # smarter event merging (i.e: looking at whether the sender is the
+        # event organizer or not, and if the sequence number got incremented).
+        if from_addr == account.email_address or from_addr == '':
+            return
 
     for part in message.attached_event_files:
         try:
